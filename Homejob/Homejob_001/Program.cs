eaef4f6356fa02b3b46ac0494f8b4f4340b50c30
Pavel <bejenar@list.ru>
2023-01-17 15:44:56 +0300

    Add System console

--- Homejob/Homejob_001/Program.cs
+++ Homejob/Homejob_001/Program.cs
@@ -5,19 +5,22 @@ Console.WriteLine("Введите второе число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1 > number2)
-
 {
+    System.Console.Write("max = ");
     System.Console.WriteLine(number1);
 }
 else if (number1 < number2)
 {
+    System.Console.Write("max = ");
     System.Console.WriteLine(number2);
 }
 if (number2 < number1)
 {
+    System.Console.Write("min = ");
     System.Console.WriteLine(number2);
 }
 else if (number2 > number1)
 {
+    System.Console.Write("min = ");
     System.Console.WriteLine(number1);
 }
