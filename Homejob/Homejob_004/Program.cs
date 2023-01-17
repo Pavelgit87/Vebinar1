db2e6402b430a0278e4ebfc39d02c1ee55463b0a
Pavel <bejenar@list.ru>
2023-01-17 18:18:16 +0300

    Add min max

---
+++ Homejob/Homejob_004/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int result = number / 2; result <= number; result = result + 2)
+{
+ System.Console.Write(result + " ");
+}
