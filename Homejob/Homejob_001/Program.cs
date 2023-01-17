e17e8ecc63347d885a20ac295d4387f18a2cc3bd
Pavel <bejenar@list.ru>
2023-01-17 13:56:05 +0300

    Add gitignore

---
+++ Homejob/Homejob_001/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+
+{
+    System.Console.WriteLine(number1);
+}
+else if (number1 < number2)
+{
+    System.Console.WriteLine(number2);
+}
+if (number2 < number1)
+{
+    System.Console.WriteLine(number2);
+}
+else if (number2 > number1)
+{
+    System.Console.WriteLine(number1);
+}
